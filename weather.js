a7a18ca6eed4a821d9293603e003783424c2cde2
Rohitadesh <adeshnagalikar2000@gmail.com>
2023-06-20 06:47:00 +0530

    inital commit

---
+++ weather.js
@@ -0,0 +1,7 @@
+let input_data=document.querySelector('.input')
+let button=document.querySelector('.button')
+let degree_text=document.querySelector('.degree');
+let city=document.querySelector('.city');
+
+
+
