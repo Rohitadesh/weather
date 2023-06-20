12d45b9cbca1e822b989b79bdee8a10ddeac3ef7
Rohitadesh <adeshnagalikar2000@gmail.com>
2023-06-20 10:55:57 +0530

    second commit

--- weather.js
+++ weather.js
@@ -1,7 +1,31 @@
-let input_data=document.querySelector('.input')
+let input_city=document.getElementById('input');
 let button=document.querySelector('.button')
-let degree_text=document.querySelector('.degree');
-let city=document.querySelector('.city');
+let temperature=document.querySelector('.temp');
+let humidity=document.querySelector('.humidity')
+let windspeed=document.querySelector('.windspeed')
+let city_name=document.querySelector('.town_name')
 
 
 
+
+const weather_cast_info= async(city)=>{
+        let apikey= "70592b526d9742b8b1bb7e8932ee97cd"
+        // let url="https://api.openweathermap.org/data/2.5/weather";
+        // let url_info=`${url}?q=${city}&APPID=${apikey}&units=metric`;
+        let url_info=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+        let response= await fetch(url_info);
+        let weather_data= await response.json();
+        weather_display(weather_data)
+}
+
+
+const weather_display=(weather_data_input)=>{
+    // console.log(weather_data_input)
+    city_name.innerHTML=weather_data_input.name
+    temperature.innerHTML=weather_data_input.main.temp +'°C'
+    humidity.innerHTML=weather_data_input.main.humidity+'%'
+    windspeed.innerHTML=weather_data_input.main.speed+"km/hr";
+}
+
+button.addEventListener('click',
+weather_cast_info(input_city.value));
